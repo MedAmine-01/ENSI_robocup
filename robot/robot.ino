68d045e0ff3a96f68468b87ab0165614c29bd977
MedAmine-01 <aminebhj27@gmail.com>
2024-10-08 21:57:08 +0100

    demo

--- robot/robot.ino
+++ robot/robot.ino
@@ -3,7 +3,8 @@
 #define lm1 9
 #define lm2 10
 
-#define ch1 2
+#define ch1 11
+#define ch2 12
 
 
 int duration1;
@@ -11,6 +12,9 @@ int duration2;
 
 int stickX;
 int stickY;
+
+int leftMotorPWM;
+int rightMotorPWM;
 
 
 void readPulse(){
@@ -26,12 +30,46 @@ void calculPWM(){
   leftMotorPWM += stickX;        
   rightMotorPWM -= stickX;   
   leftMotorPWM = constrain(leftMotorPWM,-255,255);
-  leftMotorPWM = constrain(leftMotorPWM,-255,255);   
+  rightMotorPWM = constrain(rightMotorPWM,-255,255);   
 }
 
 
+void move(){
+  if (leftMotorPWM>0){
+    analogWrite(lm1,leftMotorPWM);
+    analogWrite(lm2,0);
+  }
+  else{
+    analogWrite(lm1,0);
+    analogWrite(lm2,abs(leftMotorPWM));
+  }
+  if (rightMotorPWM>0){
+    analogWrite(rm1,rightMotorPWM);
+    analogWrite(rm2,0);
+  }
+  else{
+    analogWrite(rm1,0);
+    analogWrite(rm2,abs(rightMotorPWM));
+  }
+}
+
+
+void setup(){
+  pinMode(rm1, OUTPUT);
+  pinMode(rm2, OUTPUT);
+  pinMode(lm1, OUTPUT);
+  pinMode(lm2, OUTPUT);
+  pinMode(ch1, INPUT);
+  pinMode(ch2, INPUT);
+  Serial.begin(9600);
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  readPulse();
+  calculPWM();
+  Serial.print(rightMotorPWM);
+  Serial.print("    ");
+  Serial.println(leftMotorPWM);
+  move();
+  delay(100);
 }
